fd6b6e9bb209e30ffcc6dc5e262715d9e2f29855
Rocket_GR <georgiiromanishko@gmail.com>
2025-05-17 15:16:54 +0200

    feat(project): separate commands

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,83 @@
+use chrono::Utc;
+use serde_json::{from_value, json};
+use std::sync::Mutex;
+use tauri::{AppHandle, Manager};
+use tauri_plugin_store::StoreExt;
+
+use crate:: model;
+use crate::crypto;
+use crate::helpers;
+use crate::constants;
+
+//Get state app
+#[tauri::command]
+pub async fn get_app_state(app_handle: AppHandle) -> Result<model::UserData, String> {
+    let state = app_handle.state::<Mutex<model::AppState>>();
+
+    let state = state.lock().map_err(|e| e.to_string())?;
+
+    Ok(model::UserData::new(state.is_initialized, state.user_name.clone(), state.create_date.clone()))
+}
+
+//Sign up
+#[tauri::command]
+pub async fn sign_up(app_handle: AppHandle, name: String, password: String) -> Result<model::SignUpResponse, String> {
+    let common_store = app_handle.store(constants::STORE_PATH_COMMON).map_err(|e| e.to_string())?;
+    let protected_store = app_handle.store(constants::STORE_PATH_PROTECTED).map_err(|e| e.to_string())?;
+
+    let secure = crypto::SecureData::encrypt(&password, "{}".to_string()).await.expect("Fails to encrypt data");
+    let user_data = model::UserData::new(true, name, Utc::now().to_rfc3339());
+
+    common_store.set("data", json!(model::CommonStore::new(&user_data)));
+    protected_store.set("data", json!(secure));
+
+    let state = app_handle.state::<Mutex<model::AppState>>();
+    let mut state = state.lock().map_err(|e| e.to_string())?;
+
+    state.fill_state(&user_data);
+    state.set_password(password);
+
+    Ok(model::SignUpResponse {user_data, json_str: secure.get_secure_field() })
+}
+
+//Sign in
+#[tauri::command]
+pub async fn sign_in(app_handle: AppHandle, password: String) -> Result<model::SignInResponse, String> {
+    let protected_store = app_handle.store(constants::STORE_PATH_PROTECTED).map_err(|e| e.to_string())?;
+
+    let value = protected_store.get("data").expect("Failed to get data from protected store");
+    let secure: crypto::SecureData = from_value(value).expect("Failed to parse protected store");
+    let json_str = crypto::SecureData::decrypt(&password, secure.get_salt(), secure.get_nonce(), secure.get_secure_field()).map_err(|e| e.to_string())?;
+
+    let state = app_handle.state::<Mutex<model::AppState>>();
+    let mut state = state.lock().map_err(|e| e.to_string())?;
+
+    state.set_password(password);
+
+    let user_data = model::UserData::new(state.is_initialized, state.user_name.clone(), state.create_date.clone());
+
+    Ok(model::SignInResponse {user_data, json_str })
+}
+
+//Drop all data
+#[tauri::command]
+pub async fn drop_all_data(app_handle: AppHandle) -> Result<String, String> {
+    let state = app_handle.state::<Mutex<model::AppState>>();
+    let mut state = state.lock().map_err(|e| e.to_string())?;
+
+    let common_store = app_handle.store(constants::STORE_PATH_COMMON).map_err(|e| e.to_string())?;
+    let protected_store = app_handle.store(constants::STORE_PATH_PROTECTED).map_err(|e| e.to_string())?;
+
+    common_store.clear();
+    protected_store.clear();
+
+    state.drop_state();
+
+    Ok("The full user data was dropped".to_string())
+}
+
+//Generate random string
+#[tauri::command]
+pub fn get_random_string() -> String {
+    helpers::random_string(20)
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,88 +1,15 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-use chrono::Utc;
-use serde_json::{from_value, json};
+use serde_json::{from_value};
 use std::sync::Mutex;
-use tauri::{AppHandle, Manager};
+use tauri::{Manager};
 use tauri_plugin_store::StoreExt;
 
 mod model;
 mod helpers;
 mod constants;
 mod crypto;
-
-//Get state app
-#[tauri::command]
-async fn get_app_state(app_handle: AppHandle) -> Result<model::UserData, String> {
-    let state = app_handle.state::<Mutex<model::AppState>>();
-
-    let state = state.lock().map_err(|e| e.to_string())?;
-
-    Ok(model::UserData::new(state.is_initialized, state.user_name.clone(), state.create_date.clone()))
-}
-
-//Sign up
-#[tauri::command]
-async fn sign_up(app_handle: AppHandle, name: String, password: String) -> Result<model::SignUpResponse, String> {
-    let common_store = app_handle.store(constants::STORE_PATH_COMMON).map_err(|e| e.to_string())?;
-    let protected_store = app_handle.store(constants::STORE_PATH_PROTECTED).map_err(|e| e.to_string())?;
-
-    let secure = crypto::SecureData::encrypt(&password, "{}".to_string()).await.expect("Fails to encrypt data");
-    let user_data = model::UserData::new(true, name, Utc::now().to_rfc3339());
-
-    common_store.set("data", json!(model::CommonStore::new(&user_data)));
-    protected_store.set("data", json!(secure));
-
-    let state = app_handle.state::<Mutex<model::AppState>>();
-    let mut state = state.lock().map_err(|e| e.to_string())?;
-
-    state.fill_state(&user_data);
-    state.set_password(password);
-
-    Ok(model::SignUpResponse {user_data, json_str: secure.get_secure_field() })
-}
-
-//Sign in
-#[tauri::command]
-async fn sign_in(app_handle: AppHandle, password: String) -> Result<model::SignInResponse, String> {
-    let protected_store = app_handle.store(constants::STORE_PATH_PROTECTED).map_err(|e| e.to_string())?;
-
-    let value = protected_store.get("data").expect("Failed to get data from protected store");
-    let secure: crypto::SecureData = from_value(value).expect("Failed to parse protected store");
-    let json_str = crypto::SecureData::decrypt(&password, secure.get_salt(), secure.get_nonce(), secure.get_secure_field()).map_err(|e| e.to_string())?;
-
-    let state = app_handle.state::<Mutex<model::AppState>>();
-    let mut state = state.lock().map_err(|e| e.to_string())?;
-
-    state.set_password(password);
-    
-    let user_data = model::UserData::new(state.is_initialized, state.user_name.clone(), state.create_date.clone());
-
-    Ok(model::SignInResponse {user_data, json_str })
-}
-
-//Drop all data
-#[tauri::command]
-async fn drop_all_data(app_handle: AppHandle) -> Result<String, String> {
-    let state = app_handle.state::<Mutex<model::AppState>>();
-    let mut state = state.lock().map_err(|e| e.to_string())?;
-
-    let common_store = app_handle.store(constants::STORE_PATH_COMMON).map_err(|e| e.to_string())?;
-    let protected_store = app_handle.store(constants::STORE_PATH_PROTECTED).map_err(|e| e.to_string())?;
-
-    common_store.clear();
-    protected_store.clear();
-
-    state.drop_state();
-
-    Ok("The full user data was dropped".to_string())
-}
-
-//Generate random string
-#[tauri::command]
-fn get_random_string() -> String {
-    helpers::random_string(20)
-}
+mod commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -107,7 +34,13 @@ pub fn run() {
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_app_state, sign_up, sign_in, drop_all_data, get_random_string])
+        .invoke_handler(tauri::generate_handler![
+            commands::get_app_state,
+            commands::sign_up,
+            commands::sign_in,
+            commands::drop_all_data,
+            commands::get_random_string
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
