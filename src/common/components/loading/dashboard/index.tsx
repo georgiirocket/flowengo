e404f54dd96a4cceb320b6a655166848ecc12825
Rocket_GR <georgiiromanishko@gmail.com>
2025-05-28 21:41:04 +0200

    feat(dashboard): fallback

--- src/common/components/loading/dashboard/index.tsx
+++ src/common/components/loading/dashboard/index.tsx
@@ -1,8 +1,15 @@
 import type { FC } from "react";
-import { Skeleton } from "@heroui/react";
+import ReactLogo from "@assets/icon-sq.svg?react";
 
 const DashboardLoading: FC = () => {
-  return <Skeleton className="w-full h-full" />;
+  return (
+    <div className="w-dvw h-dvh grid place-content-center">
+      <div className="flex gap-1 items-center">
+        <ReactLogo className="size-[20px]" />
+        <p className="text-tiny">Loading...</p>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLoading;
