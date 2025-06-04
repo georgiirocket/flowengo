93063e445d953709acb4547330f35b55cea1e5bb
Rocket_GR <georgiiromanishko@gmail.com>
2025-06-04 12:37:47 +0200

    feat(dashboard): dashboard page

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -3,21 +3,38 @@ import { useProjectsCtxStore } from "@common/providers/projects";
 import { Navigate } from "react-router-dom";
 import { ROUTES } from "@common/constants/routes.ts";
 import { getLocalProjectId } from "@common/helpers/save-local-project-id.ts";
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "@heroui/button";
+import ReactLogo from "@assets/icon-sq.svg?react";
+import { openNewProjectModal } from "@layouts/dashboard/components/modals/hooks/use-new-project.ts";
 
 const Dashboard: FC = () => {
   const projectsData = useProjectsCtxStore((state) => state.projectsData);
   const projectId = getLocalProjectId(projectsData);
-
-  console.log({ projectId });
 
   if (projectId) {
     return <Navigate to={`${ROUTES.dashboard}/${projectId}`} />;
   }
 
   return (
-    <div>
-      <h1>Hello Dashboard</h1>
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        className="size-full grid place-items-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <Button
+          color="primary"
+          radius="full"
+          onPress={() => openNewProjectModal()}
+          startContent={<ReactLogo className="size-[20px]" />}
+        >
+          Create the first project
+        </Button>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
